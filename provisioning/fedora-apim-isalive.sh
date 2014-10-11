897a3a3a4c66f73a5b7a43dd47ea39061cc732a2
Ralf Claussnitzer <ralf.claussnitzer@slub-dresden.de>
2014-10-11 16:02:41 +0200

    Ensure Fedora management API is up

---
+++ provisioning/fedora-apim-isalive.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+RETRY=3
+HTTPSTATUS=0
+SOAP_MSG='<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"><SOAP-ENV:Body><m:describeRepository xmlns:m="http://fedora-commons.org/2011/07/definitions/types/"/></SOAP-ENV:Body></SOAP-ENV:Envelope>'
+HOST=$1
+
+if [ -z "$HOST" ]; then
+    echo "Missing parameter for HOST. Assuming localhost"
+    HOST="localhost"
+fi
+
+until [[ $RETRY -le 0 || "$HTTPSTATUS" -eq 200 ]]; do
+    HTTPSTATUS=`curl -sw '\n%{http_code}' --retry 3 -ufedoraAdmin:fedoraAdmin -XPOST -d "$SOAP_MSG" http://$HOST:8080/fedora/services/accessMTOM 2>/dev/null | tail -n1`
+    sleep 1
+    let RETRY=RETRY-1
+done
+
+if [ "$HTTPSTATUS" -eq 200 ]; then
+    exit 0
+else
+    exit 6
+fi
+
